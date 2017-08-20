ab9b088f4d7944538c83f7e9bc7109b27485cea5
William Murphy <willmurphy100@gmail.com>
2017-08-20 13:56:40 +0000

    add new recipe creation with test

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -4,7 +4,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
   
   def setup
     @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com")
-    @recipe = Recipe.create!(name: "vegetabel saute", description: "great veggy meal", chef: @chef)
+    @recipe = Recipe.create!(name: "vegetable saute", description: "great veggy meal", chef: @chef)
     @recipe2 = @chef.recipes.build(name: "chicken saute", description: "great chicken dish")
     @recipe2.save
   end
@@ -17,8 +17,39 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listings" do
     get recipes_path
     assert_template 'recipes/index'
-    assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
+  end
+  
+  test "should get recipes show" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
+  end
+  
+  test "create new valid recipe" do
+    get new_recipe_path
+    assert_template "recipes/new"
+    name_of_recipe = "chicken saute"
+    description_of_recipe = "add chicken, add veg, cook for 20 mins, serve meal"
+    assert_difference "Recipe.count", 1 do
+      post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe }}
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
+  end
+  
+  test "reject invalid recipe submissions" do 
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: " ", description: " " } }
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
   end
   
 end
