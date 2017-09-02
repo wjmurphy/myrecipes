2bafb79b39900d14b7912f832ab9a9f3b5896b6c
William Murphy <willmurphy100@gmail.com>
2017-09-02 14:09:15 +0000

    Live comments stream

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
     delete '/logout', to: 'sessions#destroy'
     
     resources :ingredients, except: [:destroy]
+    
+    mount ActionCable.server => 'cable'
 end
