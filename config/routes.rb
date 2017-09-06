4bd402ce707b0a836cd58b6cd0471296e4857cfc
William Murphy <willmurphy100@gmail.com>
2017-09-06 18:57:46 +0000

    tweaking cable

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
     
     resources :ingredients, except: [:destroy]
     
-    mount ActionCable.server => 'cable'
+    mount ActionCable.server => '/cable'
+    
+    get '/chat' to: 'chatrooms#show'
 end
